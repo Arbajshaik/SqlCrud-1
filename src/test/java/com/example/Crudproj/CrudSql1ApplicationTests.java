606eb3a5d2eea903e70e195968c0aeae4038e161
arbaj <shaarbaj@in.ibm.com>
2020-09-22 16:57:57 +0530

    uploaded Swagger file

---
+++ src/test/java/com/example/Crudproj/CrudSql1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Crudproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSql1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
